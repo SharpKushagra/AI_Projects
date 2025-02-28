332fb684d63760eb366815fe1303689345f48680
Kushagra Saxena <donotmesswithkushagra@gmail.com>
2025-02-28 18:19:39 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+ 
+# pip install openai 
+# if you saved the key under a different environment variable name, you can do something like:
+client = OpenAI(
+  api_key="<f541de85738c4b0fb8cd852b05d000b1>",
+)
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Cloud"},
+    {"role": "user", "content": "what is coding"}
+  ]
+)
+
+print(completion.choices[0].message.content)
